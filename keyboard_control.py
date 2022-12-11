27a970c2507ded2451c981566b5a89a29f769528
MD Sunbeam <mdsunbeam3.14@gmail.com>
2022-12-11 17:47:06 -0600

    adding key names to keyboard control

--- keyboard_control.py
+++ keyboard_control.py
@@ -1,3 +1,18 @@
 # Developed by MD-Nazmus Samin Sunbeam
-# Script used to test mouse controls
+# Script used to test keyboard controls
 # https://pyautogui.readthedocs.io/en/latest/keyboard.html
+import pyautogui
+
+# key names for the keyboard buttons
+a = 'a'; b = 'b'; c = 'c'; d = 'd'; e = 'e'; f = 'f'; g = 'g'; h = 'h'; i = 'i'
+j = 'j'; k = 'k'; l = 'l'; m = 'm'; n = 'n'; o = 'o'; p = 'p'; q = 'q'; r = 'r'
+s = 's'; t = 't'; u = 'u'; v = 'v'; w = 'w'; x = 'x'; y = 'y'; z = 'z'
+space = 'space'; enter = 'enter'; backspace = 'backspace'; tab = 'tab'
+capslock = 'capslock'; esc = 'esc'; ctrl = 'ctrl'; alt = 'alt'
+shift = 'shift'; f1 = 'f1'; num_1 = '1'; num_2 = '2'; num_3 = '3'
+num_4 = '4'; num_5 = '5'; num_6 = '6'; num_7 = '7'; num_8 = '8'
+num_9 = '9'; num_0 = '0'; num_add = '+'; num_sub = '-'; num_mul = '*'
+num_div = '/'; num_dec = '.'; cmd = 'cmd'
+
+
+
